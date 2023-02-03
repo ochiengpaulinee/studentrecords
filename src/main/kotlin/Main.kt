7ec561d737b1f7cbb59414fb7d72dc707234308c
ochiengpaulinee <ochiengpakoth@gmail.com>
2023-02-03 07:59:05 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullName: String = "Joy Kea"
+    println(fullName)
+    var age: Byte = 21
+    println(age)
+    var phoneNumber: String = "0784538120"
+    println(phoneNumber)
+    var weightKg: Byte = 59
+    println(weightKg)
+    var citizenKenyan: Boolean = false
+    println(citizenKenyan)
+
+
+
+}
